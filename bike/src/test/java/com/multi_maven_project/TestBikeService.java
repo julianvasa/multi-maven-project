25e7c0281a4b31c427209255ea22233f4a59616b
julianvasa <vasa.julian@gmail.com>
2019-09-25 23:44:51 +0200

    Code refactoring

---
+++ bike/src/test/java/com/multi_maven_project/TestBikeService.java
@@ -0,0 +1,50 @@
+package com.multi_maven_project;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.TransactionSystemException;
+
+import javax.validation.*;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestBikeService {
+
+    private static Validator validator;
+
+    @Autowired
+    private BikeService service;
+
+    @Before
+    public void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void whenCoverageWithinRange_SavedBike() {
+        Bike bike = new Bike();
+        bike.setCoverage(2000.0);
+
+        Bike savedBike = service.insert(bike);
+        assertNotNull(savedBike);
+    }
+
+    @Test(expected = TransactionSystemException.class)
+    public void whenCoverageOutOfRange_NotSavedBike() {
+        Bike bike = new Bike();
+        bike.setCoverage(200000.0);
+        service.insert(bike);
+    }
+
+    @SpringBootApplication
+    static class Configuration {
+    }
+}
---
+++ jewerly/src/main/java/com/multi_maven_project/JewelryRepository.java
@@ -0,0 +1,9 @@
+package com.multi_maven_project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JewelryRepository extends JpaRepository<Jewelry, Long> {
+
+}
